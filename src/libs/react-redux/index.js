dcfee4213be2d8e951d784ec2386c29a163d65a8
longbtf8 <longbtf8@fullstack.edu.vn>
2025-11-20 14:51:41 +0700

    update

--- src/libs/react-redux/index.js
+++ src/libs/react-redux/index.js
@@ -1,4 +1,4 @@
 import Context from "./Context.js";
 import Provider from "./Provider.jsx";
-import { useDispatch, useSelector, useStore } from "./hook.js";
+import { useDispatch, useSelector, useStore } from "./hooks.js";
 export { Context, Provider, useStore, useDispatch, useSelector };
